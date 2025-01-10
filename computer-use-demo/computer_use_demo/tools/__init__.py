421918e0b707e34a2fbc20e61f497954bd31cb77
søren <sorenrood@gmail.com>
2025-01-10 10:21:32 -0800

    modify init file

--- computer-use-demo/computer_use_demo/tools/__init__.py
+++ computer-use-demo/computer_use_demo/tools/__init__.py
@@ -2,12 +2,14 @@ from .base import CLIResult, ToolResult
 from .bash import BashTool
 from .collection import ToolCollection
 from .computer import ComputerTool
+from .computer_windows import WindowsComputerTool
 from .edit import EditTool
 
 __ALL__ = [
     BashTool,
     CLIResult,
     ComputerTool,
+    WindowsComputerTool,
     EditTool,
     ToolCollection,
     ToolResult,
